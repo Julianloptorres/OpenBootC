b369de77d8c0d6f7f7dbdec3f385b83ea0399289
Julián López <julianlop001@hotmail.com>
2023-03-09 16:44:58 +0100

    clases

---
+++ Ejericio_8/Ejercicio_8/src/Cliente.java
@@ -0,0 +1,11 @@
+class Cliente extends Persona{
+    double credito;
+
+    public double getCredito() {
+        return credito;
+    }
+
+    public void setCredito(double credito) {
+        this.credito = credito;
+    }
+}
---
+++ Ejericio_8/Ejercicio_8/src/Trabajador.java
@@ -0,0 +1,11 @@
+public class Trabajador extends Persona {
+    double salario;
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
