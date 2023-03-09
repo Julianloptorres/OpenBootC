6e3f942e0c11d66aaec770f06c0e04091bcaa66a
Julián López <julianlop001@hotmail.com>
2023-03-09 16:44:48 +0100

    Añadiendo Herencia y dos clases.

--- Ejericio_8/Ejercicio_8/src/Main.java
+++ Ejericio_8/Ejercicio_8/src/Main.java
@@ -4,12 +4,33 @@ public class Main {
         Persona persona = new Persona();
 
         persona.setNombre("Julián");
-        System.out.println("Mi nombres es: " + persona.getNombre());
+        //System.out.println("Mi nombres es: " + persona.getNombre());
 
         persona.setEdad(31);
-        System.out.println("tengo " + persona.getEdad() + " años.");
-        
+        //System.out.println("tengo " + persona.getEdad() + " años.");
+
         persona.setTelefono(5648975);
-        System.out.println("Mi número de teléfono es: " + persona.getTelefono());
+        //System.out.println("Mi número de teléfono es: " + persona.getTelefono());
+
+        Cliente cliente = new Cliente();
+
+        cliente.setNombre("Ana");
+        cliente.setEdad(28);
+        cliente.setTelefono(324654);
+        cliente.setCredito(280.50);
+        
+        System.out.println("Mi nombre es " + cliente.getNombre() + ", tengo "
+        + cliente.getEdad() +  " años. Mi número de teléfono es "+ cliente.getTelefono() + " y mi crédito es de " + cliente.getCredito());
+
+        Trabajador trabajador = new Trabajador();
+        
+        trabajador.setNombre("Carlos");
+        trabajador.setEdad(25);
+        trabajador.setSalario(2200.45);
+        trabajador.setTelefono(65465423);
+        
+        System.out.println("Mi nombre es " + trabajador.getNombre() + ", tengo "
+        + trabajador.getEdad() +  " años. Mi número de teléfono es "+ trabajador.getTelefono() + " y mi salario es de " + trabajador.getSalario());
+
     }
 }
